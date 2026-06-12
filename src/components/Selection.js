ca64fc65a83406b7a16ce2a08547c324c6f5751f
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-12 23:40:04 +0530

    2

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import '../styles/Child.css';
 const Selection = (props) => {
   const { applyColor } = props
   const [style, updateSelectionStyle] = useState({ background: '' });
