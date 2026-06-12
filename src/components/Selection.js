68d984ee496ac7f3bb2a704f6c3df61a3b435b5e
Prateek7071 <tiwariprateek123@gmail.com>
2026-06-12 21:34:19 +0530

    init

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from "react"
+const Selection = (props) => {
+  const { applyColor } = props
+  const [style, updateSelectionStyle] = useState({ background: '' });
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      Selection
+    </div>
+  )
+}
 
+export default Selection
